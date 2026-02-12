4b47508f0cf2c70bbc44e86401b3fa3998b3b4d0
vishwaraj-prog <vzala0311@gmail.com>
2026-02-12 13:52:58 +0530

    Add files via upload

---
+++ First.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+float main()
+{
+    float a,b, c, d, e,f,g;
+    a=5.3;
+    b=6.5;
+   
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    
+    printf("a+b=%d\n",c);
+    printf("a-b=%d\n",d);
+    printf("a*b=%d\n",e);
+    printf("a/b=%d\n",f);
+    printf("a%b=%d\n",g);
+    
+    return 0;
+    }
