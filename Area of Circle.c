dcf844e58767a41e86ca7e538d7f8e1042bea943
vishwaraj-prog <vzala0311@gmail.com>
2026-02-19 14:46:46 +0530

    Add files via upload

---
+++ Area of Circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    float r;
+    scanf("%f", &r);
+     float area = 3.14 * (r* r);
+    
+    printf("area of circle is: %f",area);
+    
+    return 0;
+    }
