4447fdc656b09cf1ee51a4d5bb17dd664dc408f6
vishwaraj-prog <vzala0311@gmail.com>
2026-02-19 14:45:00 +0530

    Add files via upload

---
+++ sum of 2 digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("give the value of a:");
+    scanf("%d",&a);
+    printf("give the value of b:");
+    scanf("%d",&b);
+    
+    int c=a+b;
+    
+    printf("sum of a & b is :%d", c);
+    return 0;
+    }
+    
+    
